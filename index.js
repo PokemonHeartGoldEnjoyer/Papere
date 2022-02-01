7ab3420293c9d5f98c5706b5670a2fd1ac37d168
Simakou Endri <e.simakou@donboscosandona.it>
2022-02-01 16:12:52 +0100

    xfvhzdfgjhnxftgjhnz

--- index.js
+++ index.js
@@ -5,17 +5,60 @@ let elementoDaInserire = (
     </div>
 )
 
-function Titolo() {
+function Navbar() {
     return (
-        <h1 class="mio-titolo">Guarda che bel titolo!</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Papere</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Papere</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Papere</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled Papere</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+        
     )
 }
 
+
+/*function Footer() {
+    return (
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+          <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+          </a>
+          <span class="text-muted">&copy; 2021 Company, Inc</span>
+        </div>
+    
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+          <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+          <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+        </ul>
+    </footer>
+    )
+}*/
 ReactDOM.render(
     <div>
-        <Titolo />
-        <Titolo />
-        <Titolo />
+        <Navbar />
+
     </div>,                            
     document.getElementById('root')
 )
+
